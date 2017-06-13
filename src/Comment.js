a5ba3216e6aa694428f1b616097c3dcb0cecca8c
hyip8000 <hyip@purdue.edu>
2017-06-13 16:26:40 -0400

    Toggled visibility of comments text box

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import Share from './Share.js'
 
 class Comment extends Component {
+  constructor(){
+      super()
+      this.handleClick = this.handleClick.bind(this)
+      this.state = {
+        active: false,
+      }    
+      
+  }
+
+  handleClick(ev){
+    ev.preventDefault()
+    const currentState = this.state.active
+    if (currentState){
+        //hide
+        document.getElementById("comments").style.display = 'none'
+        this.setState({active: !currentState})
+    }else{
+        //show
+        document.getElementById("comments").style.display = 'block'
+        this.setState({active: !currentState})
+    } 
+  }
+
   render() {
     return (
-        <a className="article-link" href="#">
+        <div>
+        <a className="article-link" href="#" onClick={this.handleClick}>
                 <i className="fa fa-comments-o"></i>
                 <span className="article-link-text">Comments</span>
             </a>
+            <Share />
+                <textarea
+                    type="text"
+                    rows="4"
+                    cols="50"
+                    id="comments"
+                    className="input-group-field"
+                    name="comments"
+                    placeholder="Write a comment" 
+                    style={{display: 'none'}}
+                    >
+                    </textarea>
+                </div>
     );
 }}
 
--- src/Reaction.js
+++ src/Reaction.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import './Reaction.css'
-import Comments from './Comment.js'
-import Share from './Share.js'
+import Comment from './Comment.js'
 
 class Reaction extends Component {
   render() {
     return (
         <div className="article-links">
-            <Comments />
-            <Share />
-            <div className="input-group">
-                <textarea
-                    type="text"
-                    rows="4"
-                    cols="50"
-                    className="input-group-field"
-                    name="comments"
-                    placeholder="Write a comment" 
-                    >
-                    </textarea>
-                </div>
+            <Comment />
             </div>
     );
 }}
